0a26ca684122b87c40ff95d6ed7991e93ce8ccbc
James Espie <jamesespie@faceme.com>
2021-01-21 15:54:45 +1300

    Remove template code and give it a title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bank-of-james';
+  title = 'Welcome to the Bank of James!';
 }
